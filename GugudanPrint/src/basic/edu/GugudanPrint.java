b330362944a7954fc1237b8f736296d9821b52b6
JBC <qudcjf854@gmail.com>
2024-06-27 12:20:26 +0900

    구구단 4단 작성 by jbc

--- GugudanPrint/src/basic/edu/GugudanPrint.java
+++ GugudanPrint/src/basic/edu/GugudanPrint.java
@@ -2,14 +2,22 @@ package basic.edu;
 
 public class GugudanPrint {
 	public static void main(String[] args) {
-		
-		// 작성자: Dobby,김유경
+
+		// 작성자: Dobby,김유경,조땡 
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(2 + " * " + i + " = " + (2 * i));
 		}
+
+		System.out.println();
 		
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(3 + " * " + i + " = " + (3 * i));
+		}
+
+		System.out.println();
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(4 + " * " + i + " = " + (4 * i));
 		}
 	}
 }
