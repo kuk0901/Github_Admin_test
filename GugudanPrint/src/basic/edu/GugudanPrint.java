3a7ddedde7c0ae0b46105725f4c42114e247ecf7
kmj <flashbaby2@naver.com>
2024-06-27 12:23:39 +0900

    구구단5단작성bykmj

--- GugudanPrint/src/basic/edu/GugudanPrint.java
+++ GugudanPrint/src/basic/edu/GugudanPrint.java
@@ -3,7 +3,7 @@ package basic.edu;
 public class GugudanPrint {
 	public static void main(String[] args) {
 
-		// 작성자: Dobby,김유경,조땡 
+		// 작성자: Dobby,김유경,조땡 ,김민재
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(2 + " * " + i + " = " + (2 * i));
 		}
@@ -19,5 +19,13 @@ public class GugudanPrint {
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(4 + " * " + i + " = " + (4 * i));
 		}
+		
+		System.out.println();
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(5 + " * " + i + " = " + (5 * i));
+		}
+		
+		System.out.println();
 	}
 }
