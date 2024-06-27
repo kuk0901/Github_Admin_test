e560a671c86dc2d9477c5b038e77b621f4c7648b
kyk <beastium96@naver.com>
2024-06-27 12:15:09 +0900

    구구단 3단 작성 by kyk

--- GugudanPrint/src/basic/edu/GugudanPrint.java
+++ GugudanPrint/src/basic/edu/GugudanPrint.java
@@ -3,9 +3,13 @@ package basic.edu;
 public class GugudanPrint {
 	public static void main(String[] args) {
 		
-		// 작성자: Dobby
+		// 작성자: Dobby,김유경
 		for (int i = 1; i <= 9; i++) {
 			System.out.println(2 + " * " + i + " = " + (2 * i));
+		}
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(3 + " * " + i + " = " + (3 * i));
 		}
 	}
 }
